5c4a105ad5a07682931d20f0fb6537a5351c7b9d
DESKTOP-62NBI5U\yshkw <yshkwi.chacha@gmail.com>
2023-02-24 18:35:43 +0900

    いったんぷっしゅ

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 $(function () {
-    $.ajax({
-        url: 'https://yesno.wtf/api', //アクセスするURL
-        type: 'get',//post or get
-        cache: false,        //cacheを使うか使わないかを設定
-        dataType: 'json',     //data type script・xmlDocument・jsonなど
-        // data: data,           //アクセスするときに必要なデータを記載      
-     })
-        .done(function (response) {
-            //通信成功時の処理
-            //成功したとき実行したいスクリプトを記載
-            console.log(response);
-            console.log(response.answer);
-            console.log(response.image);
+    $(".image").on("click", function () {
+
+
+        $.ajax({
+            url: 'https://yesno.wtf/api', //アクセスするURL
+            type: 'get',//post or get
+            cache: false,        //cacheを使うか使わないかを設定
+            dataType: 'json',     //data type script・xmlDocument・jsonなど
+            // data: data,           //アクセスするときに必要なデータを記載      
         })
-        .fail(function (xhr) {
-            //通信失敗時の処理
-            //失敗したときに実行したいスクリプトを記載
-        })
-        .always(function (xhr, msg) {
-            //通信完了時の処理
-            //結果に関わらず実行したいスクリプトを記載
-        });
+            .done(function (response) {
+                //通信成功時の処理
+                //成功したとき実行したいスクリプトを記載
+                console.log(response);
+                console.log(response.answer);
+                console.log(response.image);
+                // $(".image").on("click",function () {
+                    // $(".image").append(`<img src=${response.image}  alt="yesorno">`)
+                    // $(".image").append(`<p>${response.answer}</p>`)
+                    $('.yesorno').css('background-image', `url(${response.image})`);
+                    $(".yesorno").append(`<p>${response.answer}</p>`)
+                    $("p").animate({"fontSize":"30px"},)
+                    $("p").animate({"opacity":"1"},)
+                    $(".yesorno").append(`<button>${response.answer}</button>`)
+
+
+
+
+                // })
+
+            })
+            .fail(function (xhr) {
+                //通信失敗時の処理
+                //失敗したときに実行したいスクリプトを記載
+            })
+            .always(function (xhr, msg) {
+                //通信完了時の処理
+                //結果に関わらず実行したいスクリプトを記載
+            });
+    })
 })
